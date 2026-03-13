a5d098ad415056022a0979aa6dc1bfa227997e59
Thur0808 <arthur0808sophia2501@gmail.com>
2026-03-13 12:10:09 -0300

    Semaforo sem função de ligar

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const vermelho = document.getElementById("vermelho")
 const amarelo = document.getElementById("amarelo")
 const verde = document.getElementById("verde")
 
-const ligar = document.getElementById("Ligar")
 const desligar = document.getElementById("Desligar")
 
 const lento = document.getElementById("lento")
@@ -23,32 +22,30 @@ function apagar(){
 }
 
 function ciclo(){
-
     if(estado === "vermelho"){
         apagar()
         verde.classList.add("verde")
         estado = "verde"
         intervalo = setTimeout(ciclo, tempoVerde)
     }
-
     else if(estado === "verde"){
         apagar()
         amarelo.classList.add("amarelo")
         estado = "amarelo"
         intervalo = setTimeout(ciclo, tempoAmarelo)
     }
-
     else{
         apagar()
         vermelho.classList.add("vermelho")
         estado = "vermelho"
         intervalo = setTimeout(ciclo, tempoVermelho)
     }
-
 }
 
 function iniciar(){
+    clearTimeout(intervalo) // cancela ciclos anteriores
     estado = "vermelho"
+    apagar()
     vermelho.classList.add("vermelho")
     intervalo = setTimeout(ciclo, tempoVermelho)
 }
@@ -58,23 +55,26 @@ function parar(){
     apagar()
 }
 
-ligar.addEventListener("click", iniciar)
-desligar.addEventListener("click", parar)
-
+// Quando clicar em um modo, ajusta tempos e inicia
 lento.addEventListener("click", ()=>{
     tempoVermelho = 2000
     tempoVerde = 1000
     tempoAmarelo = 2000
+    iniciar()
 })
 
 normal.addEventListener("click", ()=>{
     tempoVermelho = 1000
     tempoVerde = 500
     tempoAmarelo = 1000
+    iniciar()
 })
 
 rapido.addEventListener("click", ()=>{
     tempoVermelho = 100
     tempoVerde = 50
     tempoAmarelo = 100
+    iniciar()
 })
+
+desligar.addEventListener("click", parar)
